3cc0971ec5bd8b56d5d46dcfa707e090eb2ef7f4
Arthur <101161738+DarkBBR@users.noreply.github.com>
2025-02-14 19:39:22 -0300

    Update Projects.js

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -31,35 +31,26 @@ const projects = [
 ];
 
 const Card = ({ title, subtitle, link }) => (
-      <a
-        href={link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="container group"
-      >
-  <div class="grid-container">
-    <div class="container">
-        <div className="tracker" />
-        <div id="card">
-          <div className="card-content">
-            <h2 className="title">{title}</h2>
-            <p className="subtitle">{subtitle}</p>
-            <p id="prompt">Clique para ver no GitHub</p>
-            <div className="glowing-elements">
-              <div className="glow-1"></div>
-              <div className="glow-2"></div>
-              <div className="glow-3"></div>
-            </div>
-            <div className="card-particles">
-              {[...Array(6)].map((_, index) => (
-                <span key={index}></span>
-              ))}
-            </div>
-          </div>
+  <a href={link} target="_blank" rel="noopener noreferrer" className="container group">
+    <div className="tracker" />
+    <div id="card">
+      <div className="card-content">
+        <h2 className="title">{title}</h2>
+        <p className="subtitle">{subtitle}</p>
+        <p id="prompt">Clique para ver no GitHub</p>
+        <div className="glowing-elements">
+          <div className="glow-1"></div>
+          <div className="glow-2"></div>
+          <div className="glow-3"></div>
         </div>
+        <div className="card-particles">
+          {[...Array(6)].map((_, index) => (
+            <span key={index}></span>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-      </a>
+  </a>
 );
 
 export default function ProjectsSection() {
@@ -69,16 +60,11 @@ export default function ProjectsSection() {
       <h1 className="text-5xl font-bold bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-transparent bg-clip-text mb-8">
         PROJETOS
       </h1>
-
+      
       {/* Seção dos cards */}
       <div className="flex flex-wrap justify-center gap-6">
         {projects.map((project, index) => (
-          <Card
-            key={index}
-            title={project.title}
-            subtitle={project.subtitle}
-            link={project.link}
-          />
+          <Card key={index} title={project.title} subtitle={project.subtitle} link={project.link} />
         ))}
       </div>
     </div>
